9911f92bd1d13676b5d0164a082ae028f378c4a4
Antoine-briz <antoine.brizard@outlook.fr>
2025-10-07 20:25:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -173,3 +173,16 @@ function mount() {
 function h(cls, html) {
     return `<div class="${cls}">${html}</div>`;
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('Service Worker enregistré avec succès:', registration);
+      })
+      .catch((error) => {
+        console.log('Erreur lors de l\'enregistrement du Service Worker:', error);
+      });
+  });
+}
+
