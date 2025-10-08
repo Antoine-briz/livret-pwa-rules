33fc0c1b731d4379d8057698bf1727ba5a0e1d3d
Antoine-briz <antoine.brizard@outlook.fr>
2025-10-08 16:05:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as pdfjsLib from 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js';
+import { getDocument } from 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js';
 
 let currentPage = 1;  // Page actuelle
 let pdfDoc = null;    // Référence au document PDF
