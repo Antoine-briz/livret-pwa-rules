92539948cdc88b343e54b71b2219d0359c0173d5
Antoine-briz <antoine.brizard@outlook.fr>
2025-10-09 17:27:43 +0200

    Update rules.js

--- data/rules.js
+++ data/rules.js
@@ -1,3 +1,23 @@
 
 console.log("Règles chargées");
 // Ajoutez ici toute logique supplémentaire nécessaire pour votre application PWA
+{
+  "name": "Guide pratique MIR CHV",
+  "short_name": "Livret",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#2196f3",
+  "icons": [
+    {
+      "src": "img/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "img/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
